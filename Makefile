e372b8626a466f0b4991354851d6f866746c294d
liuyang1 <liuyang1@mail.ustc.edu.cn>
2013-01-05 12:42:26 +0800

    add erlang timeout feature

---
+++ Makefile
@@ -0,0 +1,2 @@
+stat:
+	cloc . --exclude-dir=.git
--- expr/forward.erl
+++ expr/forward.erl
@@ -1,27 +1,68 @@
 -module(forward).
--export([start/0,client/1]).
+-export([start/0,client/1,forward/0,front/0,timing/0,forwardloop/1,frontloop/2]).
+
+-define(inport,1234).
+-define(outport,1235).
 
 start() ->
-	spawn(fun() -> server(1234) end).
+	register(forwarder,spawn(forward,forward,[])),
+	register(fronter,spawn(forward,front,[])),
+	register(timer,spawn(forward,timing,[])).
 
-server(Port)->
-	{ok,InSocket} = gen_udp:open(Port,[binary,{active,false}]),
-	io:format("forward server opened socket:~p~n",[InSocket]),
-	{ok,OutSocket}= gen_udp:open(Port+1,[binary,{active,false}]),
-	io:format("forward server opened socket:~p~n",[OutSocket]),
-	loop(InSocket,OutSocket).
+forward() ->
+	{ok,InSocket} = gen_udp:open(?inport,[binary]),
+	forwardloop(InSocket).
 
-loop(InSocket,OutSocket) ->
-	inet:setopts(InSocket,[{active,once}]),
-	inet:setopts(OutSocket,[{active,once}]),
+forwardloop(InSocket) ->
 	receive
-		{udp,InSocket,Host,Port,Bin} ->
-			io:format("recv:~p from (~p,~p)~n",[Bin,Host,Port]),
-			% gen_udp:send(InSocket,Host,Port,Bin),
-			gen_udp:send(OutSocket,Host,Port,Bin),
-			loop(InSocket,OutSocket)
+		{udp,InSocket,_,_,Bin} ->
+			fronter ! {dataflow,Bin},
+			forwardloop(InSocket)
 	end.
 
+front() ->
+	{ok,OutSocket}= gen_udp:open(?outport,[binary]),
+	frontloop(OutSocket,[]).
+
+frontloop(OutSocket,Addrlist) ->
+	receive
+		{udp,OutSocket,Host,Port,_} ->
+			frontloop(OutSocket,addAddrlist(Addrlist,{Host,Port}));
+		{dataflow,Bin} ->
+			sendAddrlist(Addrlist,OutSocket,Bin),
+			frontloop(OutSocket,Addrlist);
+		{timer} ->
+			frontloop(OutSocket,incTTL(Addrlist))
+	end.
+
+timing() ->
+	receive
+		_->io:format("heart mass~n")
+	after 1000->
+		fronter ! {timer},
+		timing()
+end.
+
+sendAddrlist([],_,_)-> ok;
+sendAddrlist(Lst,OutSocket,Bin)->
+	[{{Host,Port},_}|Tail]=Lst,
+	gen_udp:send(OutSocket,Host,Port,Bin),
+	sendAddrlist(Tail,OutSocket,Bin).
+
+addAddrlist([],Addr)-> [{Addr,0}];
+addAddrlist([{HAddr,HTTL}|T],Addr)->
+	if 
+		HAddr==Addr -> [{HAddr,0}|T];
+		true -> [{HAddr,HTTL}|addAddrlist(T,Addr)]
+	end.
+
+incTTL([])->[];
+incTTL([{Addr,TTL}|T])->
+	if
+		TTL>=20 -> incTTL(T);
+		true -> [{Addr,TTL+1}|incTTL(T)]
+	end.
+%%% simulator client
 client(N) ->
 	{ok,Socket} = gen_udp:open(0,[binary]),
 	io:format("client open socket ~p~n",[Socket]),
