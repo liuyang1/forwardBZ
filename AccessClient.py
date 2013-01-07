84006f2df9841562487fd2c4f63e264c2bceee28
liuyang1 <liuyang1@mail.ustc.edu.cn>
2013-01-07 16:41:22 +0800

    修改为从命令行得到参数,为转换为可执行程序做准备

--- AccessClient.py
+++ AccessClient.py
@@ -2,29 +2,37 @@
 from socket import *
 import thread
 from time import sleep
-# config content
-forward_addr=('0.0.0.0',1235)#public network IP addr
-lo_addr=('127.0.0.1',1234)
-# end of config
+from sys import argv
+import getopt
+
 sock=socket(AF_INET,SOCK_DGRAM)
 lo=socket(AF_INET,SOCK_DGRAM)
 pktlen=2048
 
-def forward():
+def forward(lo_addr):
 	while 1:
 		data,addr = sock.recvfrom(pktlen)
 		lo.sendto(data,lo_addr)
 	sock.close()
 	lo.close()
 
-def beat():
+def beat(forward_addr):
 	while 1:
 		sock.sendto("access",forward_addr)
 		sleep(10)
 
+def usage():
+	print "usage:"
+	print "\t%s RemoteIP RemotePort LocalPort"%(argv[0])
+	exit()
+
+def main():
+	if len(argv)!=4:
+		usage()
+	remote=(argv[1],int(argv[2]))
+	local=('127.0.0.1',int(argv[3]))
+	thread.start_new(beat,(remote,))
+	forward(local)
+
 if __name__=="__main__":
-	if forward_addr[0]=='0.0.0.0':
-		print "usage:\n\tmust config public network IP addr"
-		exit()
-	thread.start_new(beat,())
-	forward()
+	main()
